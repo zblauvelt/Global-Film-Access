326a87c47906a8d2136b742b6d7667e23e055702
zblauvelt <zblauvelt@hotmail.com>
2017-12-12 19:50:41 -0500

    Add-project-compete

--- Global Film Access/Controller/CreateProjectVC.swift
+++ Global Film Access/Controller/CreateProjectVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class CreateProjectVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var containerView: CustomView!
@@ -16,6 +17,7 @@ class CreateProjectVC: UIViewController, UIImagePickerControllerDelegate, UINavi
     
     let picker = UIDatePicker()
     var imagePicker: UIImagePickerController!
+    var imageSelected = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,6 +40,7 @@ class CreateProjectVC: UIViewController, UIImagePickerControllerDelegate, UINavi
         if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
             imageAdd.image = image
             imageAdd.contentMode = .scaleToFill
+            imageSelected = true
         } else {
             print("ZACK: A valid image wasn't selected.")
         }
@@ -49,21 +52,70 @@ class CreateProjectVC: UIViewController, UIImagePickerControllerDelegate, UINavi
     }
     
     
+    @IBAction func saveImageTapped(_ sender: Any) {
+        guard let projectName = projectNameLbl.text, projectName != "" else {
+            print("ZACK: User didn't add name") //TODO: add alert of date or name is blank
+            return
+        }
+        guard let date = dateLbl.text, date != "" else {
+            print("ZACK: User didn't add a release date") //TODO: add alert of date or name is blank
+            return
+        }
+        guard let img = imageAdd.image, imageSelected == true else {
+            
+            print("ZACK: An image must be selected") //TODO: If they do not provide an image provide one
+            return
+        }
+        
+        if let imgData = UIImageJPEGRepresentation(img, 0.2) {
+            
+            let imgUid = NSUUID().uuidString
+            let metaData = FIRStorageMetadata()
+            metaData.contentType = "image/jpeg"
+            
+            DataService.ds.REF_PROJECT_IMAGES.child(imgUid).put(imgData, metadata: metaData) { (metaData, error) in
+                if error != nil {
+                    print("ZACK: Unable to upload to Firebase Storage") //TODO: send alert to user
+                } else {
+                    print("ZACK: Successfully uploaded image")
+                    let downloadURL = metaData?.downloadURL()?.absoluteString
+                    if let url = downloadURL {
+                        self.addProjectToFirebase(imgURL: url)
+                    }
+                }
+            }
+            
+        }
+        
+    }
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    //Post to Firebase
+    func addProjectToFirebase(imgURL: String) {
+        let project: Dictionary<String, AnyObject> = [
+            "name": projectNameLbl.text as AnyObject,
+            "image": imgURL as AnyObject,
+            "startDate": dateLbl.text as AnyObject
+        ]
+        
+        let firebaseProject = DataService.ds.REF_PROJECTS.childByAutoId()
+        firebaseProject.setValue(project)
+        
+        let projectId = "\(firebaseProject.key)"
+        
+        let newUserProject: Dictionary<String, String> = [
+            "projectid": projectId
+        ]
+        
+        let firebaseUserProject = DataService.ds.REF_USER_PROJECTS.childByAutoId()
+        firebaseUserProject.setValue(newUserProject)
+        
+        projectNameLbl.text = nil
+        imageSelected = true
+        dateLbl.text = nil
+        imageAdd.image = UIImage(named: "ImageBtn.png")
+        imageAdd.contentMode = .center
+    }
     
     
     //Creation of datepicker
