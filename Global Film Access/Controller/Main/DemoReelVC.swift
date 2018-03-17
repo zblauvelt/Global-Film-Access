eca6a97c3d3e8135fe593487ff09f902aef253c9
zblauvelt <zblauvelt@hotmail.com>
2018-03-17 14:21:53 -0400

    DemoReelDone

--- Global Film Access/Controller/Main/DemoReelVC.swift
+++ Global Film Access/Controller/Main/DemoReelVC.swift
@@ -10,19 +10,39 @@ import UIKit
 
 class DemoReelVC: UIViewController {
     
-    var chosenVideoID = ""
+    
+    @IBOutlet weak var videoWebView: UIWebView!
+    
+    
+    var fullURL = ""
+    var chosenVideoID: String!
+    var url = "https://player.vimeo.com/video/"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         print("CHOSEN ID: \(chosenVideoID)")
+        
+        let requestURL = NSURL(string: getVideoURL(url: url, id: chosenVideoID))
+        let request = NSURLRequest(url: requestURL! as URL)
+        
+        videoWebView.loadRequest(request as URLRequest)
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    
+    func getVideoURL(url: String, id: String) -> String {
+        if let video = chosenVideoID {
+            fullURL = "\(url)\(video)"
+            print("FULL URL: \(fullURL)")
+        }
+        return fullURL
     }
+    
+    
     
 
 }
