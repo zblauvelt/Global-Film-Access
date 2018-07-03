473fdae18e92ab0846f27971fd0877e0f7f76cb9
zblauvelt <zblauvelt@hotmail.com>
2018-07-03 10:29:41 -0400

    castingDetailCellDone

---
+++ Global Film Access/Models/Notifications.swift
@@ -0,0 +1,33 @@
+//
+//  Invitations.swift
+//  Global Film Access
+//
+//  Created by Zachary Blauvelt on 4/30/18.
+//  Copyright © 2018 Zachary Blauvelt. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+enum EventLabel: String {
+    case audition = "audition"
+    case event = "event"
+}
+
+class 
+
+        
+        /*let invite: Dictionary <String, String> = [
+            FIREventData.message.rawValue: event.message,
+            FIREventData.profileImage.rawValue: invitorProfileImage,
+            FIREventData.invitor.rawValue: invitorName,
+            FIREventData.projectID.rawValue: projectID,
+            FIREventData.auditionID.rawValue: auditionID,
+            FIREventData.position.rawValue: positionName
+            
+        ]
+        EventInvite.REF_EVENT_INVITE.child(inviteeKey).childByAutoId().setValue(invite)*/
+    
+    
+    
+}
